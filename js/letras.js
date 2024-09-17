e53340af33d0224a1cad5949d832e41b3fdc0166
Mrjoga <163124772+Mrjoga@users.noreply.github.com>
2024-09-17 08:39:04 -0300

    .

---
+++ js/letras.js
@@ -0,0 +1,58 @@
+function exercicio1(){
+    document.getElementById("respostaexercicio1").innerHTML = 
+            'Uma casa branca, um quarto branco <br>'
+            'O progama de hoje <br>'
+            'Iluminar, acender <br>'
+            'Seus olhos estão distantes <br><br>'
+        
+            //   The map represents you <br>
+            //   And the tape is your voice <br>
+            //   Follow all along you <br>
+            //   'Til you recognize the choice <br><br>
+        
+            //   I take pictures <br>
+            //   Photographic pictures <br><br>
+        
+            //   Bright light, dark room <br>
+            //   Bright light, dark room <br><br>
+        
+            //   I said I'd write a letter <br>
+            //   But I never got the time <br>
+            //   And looking to the day <br>
+            //   I mesmerize the light <br><br>
+        
+            //   The years I spend just thinking <br>
+            //   Of a moment we both knew <br>
+            //   A second boss looking into <br>
+            //   It seems it can't be true <br><br>
+        
+            //   I take pictures <br>
+            //   Photographic pictures <br><br>
+        
+            //   Bright light, dark room <br>
+            //   Bright light, dark room <br><br>
+        
+            //   I take pictures <br>
+            //   Photographic pictures <br><br>
+        
+            //   Bright light, dark room <br>
+            //   Bright light, dark room <br><br>
+        
+            //   Bright light, dark room <br>
+            //   Bright light, dark room <br><br>
+        
+            //   Bright light, dark room <br>
+            //   Bright light, dark room <br><br>
+        
+            //   Bright light, dark room <br>
+            //   Bright light, dark room <br><br>
+        
+            //   Bright light, dark room <br>
+            //   Bright light, dark room <br><br>
+        
+            //   Bright light, dark room <br>
+            //   Bright light, dark room <br><br>
+        
+            //   Bright light, dark room <br>
+            //   Bright light, dark room;
+}
